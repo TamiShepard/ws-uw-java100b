dffcfcfad01f17e9603613085b8b1330a3a66c1b
TamiShepard <tmshepard1@gmail.com>
2023-03-20 19:34:47 -0700

    Ass01 - create business card

---
+++ ass01/src/main/java/edu/uw/java100b/BusinessCard.java
@@ -0,0 +1,18 @@
+package edu.uw.java100b;
+
+public class BusinessCard {
+    public static void main(String [] args) {
+        System.out.println(" ");
+        System.out.println("+----------------------------------------+");
+        System.out.println("|                                        |");
+        System.out.println("|        University of Washington        |");
+        System.out.println("|                                        |");
+        System.out.println("|              Tami Shepard              |");
+        System.out.println("|         Professional Cat Herder        |");
+        System.out.println("|                                        |");
+        System.out.println("|              360-555-1212              |");
+        System.out.println("|                                        |");
+        System.out.println("+----------------------------------------+");
+        System.out.println(" ");
+    }
+}
